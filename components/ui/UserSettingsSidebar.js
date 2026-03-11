9b2619023aba325f8bde077217fb7e2b53b1813f
Bogdan Mironov <mironovbogdan1354@gmail.com>
2026-03-11 10:05:05 +0300

    feat: add separator before API tokens in user settings sidebar

--- components/ui/UserSettingsSidebar.js
+++ components/ui/UserSettingsSidebar.js
@@ -61,6 +61,8 @@ export default function UserSettingsSidebar({ user, labels, profileIconAlt }) {
                     {labels.settings}
                 </Link>
 
+                <div className="sidebar-separator-view _theme_default _size_s"></div>
+
                 <Link href="/settings/api" className={`sidebar-item ${isActive("/settings/api") ? "sidebar-item--active" : ""}`} data-ripple>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon lucide lucide-braces-icon lucide-braces">
                         <path d="M8 3H7a2 2 0 0 0-2 2v5a2 2 0 0 1-2 2 2 2 0 0 1 2 2v5c0 1.1.9 2 2 2h1"/>

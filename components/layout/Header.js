7add7429cd4bcbde2e8e47360e71e0035f56e64c
Bogdan Mironov <mironovbogdan1354@gmail.com>
2026-04-15 06:17:23 +0300

    feat: change "Create Project" button style to secondary

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -233,7 +233,7 @@ export default function Header({ authToken }) {
                     <div className="header__right">
                         {isLoggedIn ? (
                             <>
-                                <button className="button button--size-m button--type-primary button--active-transform button--with-icon" onClick={openProjectModal}>
+                                <button className="button button--size-m button--type-secondary button--active-transform button--with-icon" onClick={openProjectModal}>
                                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" className="icon lucide lucide-plus-icon lucide-plus">
                                         <path d="M5 12h14"/>
                                         <path d="M12 5v14"/>
